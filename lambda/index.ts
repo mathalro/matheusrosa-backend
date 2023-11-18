ebac76e636fe4fc2daada5c641fa71147a0cf61d
Matheus Rosa <matheus.ar11@hotmail.com>
2023-11-18 20:29:27 +0000

    Integrating with Dynamo DB

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,5 +1,46 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from 'aws-lambda';
+import { DynamoDBClient, ScanCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+
+const ddbClient = new DynamoDBClient({});
+const ddb = DynamoDBDocumentClient.from(ddbClient);
+const tableName = "articles";
 
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    return { statusCode: 200, body: 'Hello World!' };
+    let response: APIGatewayProxyResult;
+
+    let module = event.path.split("/").at(0);
+
+    try {
+        switch (module) {
+            case "articles":
+                return articlesHandler(event); 
+            default:
+                response.statusCode = 404;
+                response.body = "Invalid Path";
+        }
+    } catch (err) {
+        response.statusCode = 500;
+        response.body = "Unexpected Error";
+    }
+
+    return response;
 };
+
+const articlesHandler = async(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    let response: APIGatewayProxyResult;
+    let method = event.httpMethod.toLowerCase();
+
+    switch (method) {
+        case "get": {
+            let body = await ddb.send(new ScanCommand( {
+                TableName: tableName
+            }));
+
+            response.statusCode = 200;
+            response.body = body.Items.toString();
+            
+            return response;
+        }
+    }
+}
